36c4d7c56cea0b2948d2a01e457dfc714c8c858d
Eugeny Nosenko <info@coders.studio>
2022-02-26 12:13:45 +0300

    Alpha Version 0.0.1 Done!

---
+++ main.py
@@ -0,0 +1,41 @@
+from pyclickup import ClickUp
+import os
+from dotenv import load_dotenv
+from flask import Flask, json
+
+api = Flask(__name__)
+
+
+@api.route('/api/v1/tasks', methods=['GET'])
+def get_companies():
+    load_dotenv()
+    api_key: str = os.getenv('API_KEY', '')
+    team_id: int = int(os.getenv('TEAM', 0))
+    space_id: int = int(os.getenv('SPACE', 0))
+    project_id: int = int(os.getenv('PROJECT', 0))
+    list_id: int = int(os.getenv('LIST', 0))
+
+    clickup = ClickUp(api_key)
+    main_team = clickup.teams[team_id]
+    main_space = main_team.spaces[space_id]
+
+    main_project = main_space.projects[project_id]
+    main_list = main_project.lists[list_id]
+
+    tasks = main_list.get_all_tasks(include_closed=False)
+
+    result: list = []
+
+    for task in tasks:
+        item = {
+            'id': task.id,
+            'name': task.name,
+            'description': task.description,
+            'url': task.url
+        }
+        result.append(item)
+    return json.dumps(result)
+
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=int(os.getenv('API_PORT', 8111)))
